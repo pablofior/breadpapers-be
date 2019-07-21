49ebbc1a18da839b6cfa74a372132232c7db1945
Pablo Fior <pabloribasfior@gmail.com>
2019-07-20 21:18:52 -0300

    removendo Auth::Routes() de api.php

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Auth::routes();
-
 Route::middleware('auth:api')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
